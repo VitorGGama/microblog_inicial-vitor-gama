9e2df7dfc325c1741bbd9e6b32dc84e44491450f
vitor.gama <vitorggam@hotmail.com>
2023-10-10 11:43:29 -0300

    creacao utilitarios

---
+++ src/Utilitarios.php
@@ -0,0 +1,15 @@
+<?php
+namespace Microblog;
+abstract class Utilitarios {
+
+    /*Sobre o parametro $dados com tipo array/bool
+    Quando um parametro pode receber tipos de dados
+    diferentes de acordo com a chamada do metodo, 
+    usamos o operador | (ou) entre as opções de tipos. 
+    Essa sintax é válida a partir do php 7.4 */
+    public static function dump(array | bool $dados):void {
+        echo "<pre>";
+        var_dump($dados);
+        echo "</pre>";
+    }
+}
