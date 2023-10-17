c5a00ba9be69e2b57988420b7e976c1486580669
vitor.gama <vitorggam@hotmail.com>
2023-10-17 09:27:33 -0300

    Metodo login

--- src/ControleDeAcesso.php
+++ src/ControleDeAcesso.php
@@ -25,4 +25,14 @@ final class ControleDeAcesso
             die(); //ou exit;
         }
     }
+
+    public function login(int $id, string $nome, string $tipo):void {
+        /*No momento em que ocorre login, criamos variaveis
+        de sessão contendo os dados que queremos monitorar/controlar/usar 
+        através da sessão enquanto a pessoa estiver logada. */
+        $_SESSION["id"] = $id;
+        $_SESSION["nome"] = $nome;
+        $_SESSION["tipo"] = $tipo;       
+
+    }
 }
