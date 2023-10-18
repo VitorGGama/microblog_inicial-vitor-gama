5f04e330f855d76b717f2a87afb7c7c7ffd3b998
vitor.gama <vitorggam@hotmail.com>
2023-10-18 12:01:06 -0300

    pagina atualiza.php

--- src/Categoria.php
+++ src/Categoria.php
@@ -15,16 +15,19 @@ class Categoria
         $this->conexao = Banco::conecta();
     }
 
-    public function inserir(): void 
+    public function inserir(): void
     {
         $sql = "INSERT INTO categorias(nome)
         VALUES(:nome)";
 
         try {
             $consulta =
-            $this->conexao->prepare($sql);
-            $consulta->bindValue(":nome",
-            $this->nome, PDO::PARAM_STR);
+                $this->conexao->prepare($sql);
+            $consulta->bindValue(
+                ":nome",
+                $this->nome,
+                PDO::PARAM_STR
+            );
             $consulta->execute();
         } catch (Exception $erro) {
             die("Erro ao inserir categoria" . $erro->getMessage());
@@ -37,14 +40,27 @@ class Categoria
     {
         return $this->nome;
     }
-    
+
     public function setNome(string $nome): self
     {
         $this->nome = filter_var($nome, FILTER_SANITIZE_SPECIAL_CHARS);
 
         return $this;
     }
-}
 
-//UPDATE de categorias
-public function atualizar
+
+    //UPDATE de categorias
+    public function atualizar(): void
+    {
+        $sql = "UPDATE categorias SET nome = :nome WHERE id = :id";
+
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->bindValue(":id", $this->id, PDO::PARAM_INT);
+            $consulta->bindValue(":nome", $this->nome, PDO::PARAM_STR);
+            $consulta->execute();
+        } catch (Exception $erro) {
+            die("Erro ao atualizar categoria " . $erro->getMessage());
+        }
+    }
+}
