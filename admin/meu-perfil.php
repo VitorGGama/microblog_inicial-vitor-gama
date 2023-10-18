1eced7242d0a8f75e42640cc37d5bc0d1a7c86ca
vitor.gama <vitorggam@hotmail.com>
2023-10-18 10:40:34 -0300

    conclusao do meu perfil

--- admin/meu-perfil.php
+++ admin/meu-perfil.php
@@ -1,25 +1,56 @@
-<?php 
+<?php
+
+use Microblog\Usuario;
+use Microblog\Utilitarios;
+
 require_once "../inc/cabecalho-admin.php";
+$usuario = new Usuario;
+
+//Atribuimos ao objeto ID do usuario logado na sessão.
+$usuario->setId($_SESSION["id"]);
+
+$dados = $usuario->listarUm();
+
+if (isset($_POST["atualizar"])) {
+	$usuario->setNome($_POST['nome']);
+	$usuario->setEmail($_POST['email']);
+	$usuario->setTipo($_SESSION['tipo']); //mantendo o tipo existente
+
+	if (empty($_POST["senha"])) {
+		$usuario->setSenha($dados["senha"]);
+	} else {
+		$usuario->setSenha($usuario->verificaSenha($_POST["senha"], $dados["senha"]));
+	}
+
+	$usuario->atualizar();
+
+	//Atualizando a variavel de sessão com o novo nome
+	$_SESSION["nome"] = $usuario->getNome();
+	header("location:index.php?perfil_atualizado");
+}
+
+
+
 ?>
 
 
 <div class="row">
 	<article class="col-12 bg-white rounded shadow my-1 py-4">
-		
+
 		<h2 class="text-center">
-		Atualizar meus dados
+			Atualizar meus dados
 		</h2>
-				
+
 		<form class="mx-auto w-75" action="" method="post" id="form-atualizar" name="form-atualizar">
 
 			<div class="mb-3">
 				<label class="form-label" for="nome">Nome:</label>
-				<input class="form-control" type="text" id="nome" name="nome" required>
+				<input class="form-control" type="text" id="nome" name="nome" value="<?=$dados["nome"]?>" required >
 			</div>
 
 			<div class="mb-3">
 				<label class="form-label" for="email">E-mail:</label>
-				<input class="form-control" type="email" id="email" name="email" required>
+				<input class="form-control" type="email" id="email" name="email" value="<?=$dados["email"]?>" required>
 			</div>
 
 			<div class="mb-3">
@@ -29,12 +60,11 @@ require_once "../inc/cabecalho-admin.php";
 
 			<button class="btn btn-primary" name="atualizar"><i class="bi bi-arrow-clockwise"></i> Atualizar</button>
 		</form>
-		
+
 	</article>
 </div>
 
 
-<?php 
+<?php
 require_once "../inc/rodape-admin.php";
 ?>
-
