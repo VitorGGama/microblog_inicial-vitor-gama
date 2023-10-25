6f82f8d6eb46dc6015cfdef1411d649317e46209
vitor.gama <vitorggam@hotmail.com>
2023-10-25 10:50:01 -0300

    fazendo aparecer as noticias

--- admin/noticias.php
+++ admin/noticias.php
@@ -1,8 +1,9 @@
-<?php 
+<?php
 require_once "../inc/cabecalho-admin.php";
 
 use Microblog\Noticia;
 use Microblog\Utilitarios;
+
 $noticia = new Noticia;
 
 
@@ -13,64 +14,74 @@ $noticia->usuario->setId($_SESSION['id']);
 $noticia->usuario->setTipo($_SESSION['tipo']);
 
 $listaDeNoticias = $noticia->listar();
-Utilitarios::dump($listaDeNoticias);
+
 
 ?>
 
 
 <div class="row">
 	<article class="col-12 bg-white rounded shadow my-1 py-4">
-		
+
 		<h2 class="text-center">
-		Notícias <span class="badge bg-dark">X</span>
+			Notícias <span class="badge bg-dark"><?php count($listaDeNoticias) ?></span>
 		</h2>
 
 		<p class="text-center mt-5">
 			<a class="btn btn-primary" href="noticia-insere.php">
-			<i class="bi bi-plus-circle"></i>	
-			Inserir nova notícia</a>
+				<i class="bi bi-plus-circle"></i>
+				Inserir nova notícia</a>
 		</p>
-				
+
 		<div class="table-responsive">
-		
+
 			<table class="table table-hover">
 				<thead class="table-light">
 					<tr>
-                        <th>Título</th>
-                        <th>Data</th>
-                        <th>Autor</th>
-						<th class="text-center">Operações</th>
+						<th>Título</th>
+						<th>Data</th>
+						<?php if ($_SESSION["tipo"] === "admin") { ?>
+							<th>Autor</th>
+						<?php } ?>
+
+						<th class="text-center">Destaque</th>
+						<th class="text-center" colspan="2">Operações</th>
 					</tr>
 				</thead>
 
 				<tbody>
+					<?php foreach ($listaDeNoticias as $itemNoticia) { ?>
 
-					<tr>
-                        <td> Título da notícia... </td>
-                        <td> 21/12/2112 21:12 </td>
-                        <td> Autor da notícia... </td>
-						<td class="text-center">
-							<a class="btn btn-warning" 
-							href="noticia-atualiza.php">
-							<i class="bi bi-pencil"></i> Atualizar
-							</a>
-						
-							<a class="btn btn-danger excluir" 
-							href="noticia-exclui.php">
-							<i class="bi bi-trash"></i> Excluir
-							</a>
-						</td>
-					</tr>
+						<tr>
+							<td> <?= $itemNoticia['titulo'] ?> </td>
+							<td> <?= $itemNoticia['data'] ?></td>
 
-				</tbody>                
+							<?php if ($_SESSION["tipo"] === "admin") { ?>
+								<td> <?= $itemNoticia['autor'] ?> </td>
+							<?php } ?>
+
+							<td><?= $itemNoticia['destaque'] ?></td>
+
+							<td class="text-center">
+								<a class="btn btn-warning" href="noticia-atualiza.php">
+									<i class="bi bi-pencil"></i> Atualizar
+								</a>
+							</td>
+							<td>
+								<a class="btn btn-danger excluir" href="noticia-exclui.php">
+									<i class="bi bi-trash"></i> Excluir
+								</a>
+							</td>
+						</tr>
+					<?php } ?>
+
+				</tbody>
 			</table>
-	</div>
-		
+		</div>
+
 	</article>
 </div>
 
 
-<?php 
+<?php
 require_once "../inc/rodape-admin.php";
 ?>
-
