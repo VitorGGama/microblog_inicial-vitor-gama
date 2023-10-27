6d6515bf553b8c86397e2cc3d4b9212897db53dd
vitor.gama <vitorggam@hotmail.com>
2023-10-27 09:21:39 -0300

    lista Por Categoria

--- src/Noticia.php
+++ src/Noticia.php
@@ -302,25 +302,25 @@ final class Noticia
                     noticias.id,
                     noticias.titulo,
                     noticias.data,
-                    noticia.resumo,
+                    noticias.resumo,
                     usuarios.nome AS autor,
                     categorias.nome AS categoria
                     FROM noticias
                         INNER JOIN usuarios ON noticias.usuario_id = usuarios.id
-                        INNER JOIN categorias ON noticias.categoria_id = categoria.id                    
-                    WHERE noticias.categoria.id = :categoria.id";
+                        INNER JOIN categorias ON noticias.categoria_id = categorias.id                    
+                    WHERE noticias.categoria_id = :categoria_id";
 
         try {
             $consulta = $this->conexao->prepare($sql);
-            $consulta->bindValue(":id", $this->id, PDO::PARAM_INT);
+            $consulta->bindValue(":categoria_id", $this->categoria->getId(), PDO::PARAM_INT);
             $consulta->execute();
-            $resultado = $consulta->fetch(PDO::FETCH_ASSOC);
+            $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
         } catch (Exception $erro) {
             die("Erro ao listar categoria " .$erro->getMessage());
         }
         return $resultado;
     }
-}
+    
 
 
 
@@ -427,4 +427,5 @@ final class Noticia
         $this->termo = filter_var($termo, FILTER_SANITIZE_SPECIAL_CHARS);
         return $this;
     }
+
 }
