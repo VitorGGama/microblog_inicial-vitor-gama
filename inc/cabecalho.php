ecf56163b60522a123c41ba2161d9aea24ac67db
vitor.gama <vitorggam@hotmail.com>
2023-10-27 12:00:39 -0300

    javascript

--- inc/cabecalho.php
+++ inc/cabecalho.php
@@ -56,11 +56,16 @@ $listaDeCategorias = $noticia->categoria->listar();
               <a class="nav-link" href="admin/index.php"><i class="bi bi-lock-fill"></i> Área administrativa</a>
             </li>
           </ul>
+          <div class="position-relative">
+            <form autocomplete="off" class="d-flex" action="resultados.php" method="POST" onsubmit="return false" id="form-busca">
 
-          <form autocomplete="off" class="d-flex" action="resultados.php" method="GET">
-            <input name="busca"  class="form-control me-2" type="search" placeholder="Pesquise aqui" aria-label="Pesquise aqui">
-            <button class="btn btn-outline-secondary my-2 my-sm-0" type="submit">OK</button>
-          </form>
+              <input id="campo-busca" name="busca" class="form-control me-2" type="search" placeholder="Pesquise aqui" aria-label="Pesquise aqui">
+              <div id="resultados" class="mt-3 position-absolute container bg-white shadow-lg p-3">
+                
+              </div>
+
+                 </form>
+          </div>
         </div>
       </div>
     </nav>
--- inc/rodape.php
+++ inc/rodape.php
@@ -11,6 +11,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
+<script src="js/busca.js"></script>
 
 <?php
 /* finalizando o Output Buffer (gerenciamento de memória de saída) */
